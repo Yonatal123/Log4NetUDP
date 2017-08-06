47c0cf2daa7b9202b6cc8da3c0f0b29275c1748c
Yonatal123 <ytarlovs@gmail.com>
2017-08-06 17:06:30 +0300

    Add files via upload

---
+++ Log4NetUdpReceiver/Log4NetUdpReceiver/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Log4NetUdpReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            UdpClient udpClient;
+            byte[] buffer;
+            string loggingEvent;
+
+            try
+            {
+                udpClient = new UdpClient(8080);
+
+                while (true)
+                {
+                    buffer = udpClient.Receive(ref remoteEndPoint);
+                    loggingEvent = System.Text.Encoding.ASCII.GetString(buffer);
+                    Console.WriteLine(loggingEvent);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+        }
+    }
+}
